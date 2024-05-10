1c86d7eac169b8d707624f4cb7fdddf7a3e003f2
Riley Jenum <riley.jenum@gmail.com>
2024-05-10 21:47:51 +0200

    Initial commit

---
+++ BookNook/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BookNook
+//
+//  Created by Riley Jenum on 10/05/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}

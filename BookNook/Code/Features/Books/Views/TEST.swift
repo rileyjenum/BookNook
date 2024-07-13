1d7b2a379caeafb098459887d7292f0c2173e3d1
Riley Jenum <riley.jenum@gmail.com>
2024-07-13 19:22:13 +0200

    small update to rotation

--- BookNook/Code/Features/Books/Views/TEST.swift
+++ BookNook/Code/Features/Books/Views/TEST.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct TestView: View {
     @State private var isRotated = false
+    @State private var isScaleEnabled = false
     @Namespace private var cubeNS
     
     var degrees: Double {
@@ -21,37 +22,37 @@ struct TestView: View {
     
     var body: some View {
         VStack {
-                Color.orange
-                    .frame(width: 200, height: 200)
-                    .rotation3DEffect(
-                        .degrees(-degrees + 90),
-                        axis: (x: 1.0, y: 0.0, z: 0.0),
-                        anchor: .bottom,
-                        perspective: 0.25
-                    )
-                    .matchedGeometryEffect(id: "cube", in: cubeNS, properties: .position, anchor: .bottom, isSource: false)
-                    .scaleEffect(1 + sin(radians) * 0.42) // <-- 🤔
-                    .onTapGesture {
-                        withAnimation(.easeInOut(duration: 1.0)) {
-                            isRotated.toggle()
-                        }
+            Color.orange
+                .frame(width: 200, height: 200)
+                .matchedGeometryEffect(id: "cube", in: cubeNS, properties: .position, anchor: .leading, isSource: false)
+                .rotation3DEffect(
+                    .degrees(-degrees + 90),
+                    axis: (x: 0.0, y: 1.0, z: 0.0),
+                    anchor: .leading,
+                    perspective: 0.25
+                )
+                .scaleEffect(1 + sin(radians) * 0.38) // <-- 🤔
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 1.0)) {
+                        isRotated.toggle()
                     }
-
-                Color.red
-                    .frame(width: 200, height: 200)
-                    .matchedGeometryEffect(id: "cube", in: cubeNS, properties: .position, anchor: .top, isSource: true)
-                    .rotation3DEffect(
-                        .degrees(-degrees),
-                        axis: (x: 1.0, y: 0.0, z: 0.0),
-                        anchor: .bottom,
-                        perspective: 0.25
-                    )
-                    .onTapGesture {
-                        withAnimation(.easeInOut(duration: 1.0)) {
-                            isRotated.toggle()
-                        }
+                }
+            
+            Color.red
+                .frame(width: 200, height: 200)
+                .matchedGeometryEffect(id: "cube", in: cubeNS, properties: .position, anchor: .trailing, isSource: true)
+                .rotation3DEffect(
+                    .degrees(-degrees),
+                    axis: (x: 0.0, y: 1.0, z: 0.0),
+                    anchor: .leading,
+                    perspective: 0.25
+                )
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 1.0)) {
+                        isRotated.toggle()
                     }
-
+                }
+            
         }
     }
 }

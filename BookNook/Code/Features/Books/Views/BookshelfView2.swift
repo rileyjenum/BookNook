0e1cdf1b0f3e19c0019d14c42ddb0ab461bd2a2d
Riley Jenum <riley.jenum@gmail.com>
2024-09-15 19:28:00 +0200

    updated book shelf view to use matched geometry effect to transition into book detail view
    It is buggy with quick taps. Something to do with how selectedBook is managed

--- BookNook/Code/Features/Books/Views/BookshelfView2.swift
+++ BookNook/Code/Features/Books/Views/BookshelfView2.swift
@@ -11,39 +11,73 @@ import SwiftData
 struct BookshelfView2: View {
     
     @Query(sort: [SortDescriptor(\Book.title)]) private var queriedBooks: [Book]
-
     
     let category: BookCategory
     
+    @State var isBookDetailViewOpen: Bool = false
     
-
+    @State var selectedBook: Book?
+    
+    @Namespace private var bookAnimation
+    
     var body: some View {
         VStack {
-            Text(category.rawValue)
-                .font(.system(.title2, design: .monospaced, weight: .bold))
-                .padding(.top, 50)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack {
-                    ForEach(queriedBooks) { book in
-                        BookCoverView(book: book)
-                            .frame(width: 200, height: 300)
-                            .scrollTransition(.animated.threshold(.visible(0.9))) { content, phase in
-                                content
-                                    .scaleEffect(phase.isIdentity ? 1.3 : 0.75)
-                                    .blur(radius: phase.isIdentity ? 0 : 5)
-                                    .rotation3DEffect(Angle(degrees: phase.isIdentity ? 0 : (phase == .bottomTrailing ? 40 : -40)), axis: (x: 0, y: 1.0, z: 0))
-                            }
+            if !isBookDetailViewOpen {
+                Text(category.rawValue)
+                    .font(.system(.title2, design: .monospaced, weight: .bold))
+                    .padding(.top, 50)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        ForEach(queriedBooks) { book in
+                            BookCoverView(book: book)
+                                .frame(width: 200, height: 300)
+                                .scrollTransition(.animated.threshold(.visible(0.9))) { content, phase in
+                                    content
+                                        .scaleEffect(phase.isIdentity ? 1.0 : 0.75)
+                                        .blur(radius: phase.isIdentity ? 0 : 5)
+                                        .rotation3DEffect(Angle(degrees: phase.isIdentity ? 0 : (phase == .bottomTrailing ? 40 : -40)), axis: (x: 0, y: 1.0, z: 0))
+                                }
+                                .matchedGeometryEffect(id: book.id, in: bookAnimation)
+                                .onTapGesture {
+                                    withAnimation(.spring()) {
+                                        selectedBook = book
+                                        isBookDetailViewOpen.toggle()
+                                        
+                                    }
+                                }
+                        }
+                    }
+                    .scrollTargetLayout()
+                    .padding(.horizontal, UIScreen.main.bounds.width / 2 - 100)
+                }
+                .scrollTargetBehavior(.viewAligned)
+            } else {
+                ZStack(alignment: .top) {
+                    Color.white
+                    
+                    if let selectedBook = selectedBook {
+                        VStack(spacing: 20) {
+                            BookCoverView(book: selectedBook)
+                                .frame(width: 200, height: 300)
+                                .matchedGeometryEffect(id: selectedBook.id, in: bookAnimation)
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.top, 100)
                     }
                 }
-                .scrollTargetLayout()
-                .padding(.horizontal, UIScreen.main.bounds.width / 2 - 100)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        isBookDetailViewOpen.toggle()
+                        selectedBook = nil
+
+                    }
+                }
             }
-            .scrollTargetBehavior(.viewAligned)
         }
     }
 }
-
 
 #Preview {
     let config = ModelConfiguration(isStoredInMemoryOnly: true)
--- BookNook/Code/Features/Common/Views/Playground.swift
+++ BookNook/Code/Features/Common/Views/Playground.swift
@@ -8,34 +8,102 @@
 import SwiftUI
 
 struct Playground: View {
-    @State private var selectedIndex: Int? = nil
+
+    @State private var isProfileExpanded = false
+    @Namespace private var profileAnimation
+    @Namespace private var profileName
+    @Namespace private var profileAvatar
+    @Namespace private var profileJob
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 20) {
-                ForEach(0..<10) { index in
-                    Text("Item \(index)")
-                        .foregroundStyle(.white)
-                        .font(.largeTitle)
-                        .frame(width: 200, height: 200)
-                        .background(.red)
-                        .offset(y: selectedIndex == index ? -300 : 0) // Adjust the offset value as needed
-                        .onTapGesture {
-                            withAnimation {
-                                selectedIndex = (selectedIndex == index) ? nil : index
-                            }
-                        }
+        VStack {
+            if isProfileExpanded {
+                expandedProfileView
+            } else {
+                collapsedProfileView
+            }
+            videoList
+        }
+    }
+
+    var collapsedProfileView: some View {
+        HStack {
+            profileImage
+                .matchedGeometryEffect(id: profileAvatar, in: profileAnimation)
+                .frame(width: 60, height: 60)
+
+            VStack(alignment: .leading) {
+                Text("Profile Name")
+                    .font(.title).bold()
+                    .matchedGeometryEffect(id: profileName, in: profileAnimation)
+
+                Text("iOS Developer")
+                    .foregroundColor(.secondary)
+                    .matchedGeometryEffect(id: profileJob, in: profileAnimation)
+            }
+
+            Spacer()
+        }
+        .padding()
+    }
+
+    var expandedProfileView: some View {
+        VStack {
+            profileImage
+                .matchedGeometryEffect(id: profileAvatar, in: profileAnimation)
+                .frame(width: 130, height: 130)
+
+            VStack {
+                Text("Profile Name")
+                    .font(.title).bold()
+                    .matchedGeometryEffect(id: profileName, in: profileAnimation)
+
+                Text("iOS Developer")
+                    .foregroundColor(.pink)
+                    .matchedGeometryEffect(id: profileJob, in: profileAnimation)
+
+                Text("Check this Cool description, Check this Cool description, Check this Cool description.")
+                    .padding()
+            }
+        }
+        .padding()
+    }
+
+    var profileImage: some View {
+        Image(.homeTabIcon)
+            .resizable()
+            .clipShape(Circle())
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    isProfileExpanded.toggle()
                 }
             }
-            .frame(width: .infinity, height: 800)
-            .ignoresSafeArea(.all)
-            .background(.blue)
+    }
+
+    var videoList: some View {
+        List {
+            ForEach(0...5, id: \.self) { _ in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(height: 180)
+                        .foregroundColor(.gray.opacity(0.2))
+
+                    Image(systemName: "play.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .opacity(0.3)
+                }
+                .padding(.vertical)
+
+            }
+            .listRowSeparator(.hidden)
         }
+        .listStyle(.plain)
     }
 }
 
-struct Playground_Previews: PreviewProvider {
+struct Playground_previews: PreviewProvider {
     static var previews: some View {
-        Playground()
+        Playground().preferredColorScheme(.light)
     }
 }
